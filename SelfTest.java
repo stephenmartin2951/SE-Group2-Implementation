675235f376e832eeb952b02414f6ce08ae9306ea
Stephen <stephenmartin2951@pm.me>
2022-12-11 18:39:48 -0600

    Project complete. Integrated Rihab's changes as well.

--- SelfTest.java
+++ SelfTest.java
@@ -1,10 +1,8 @@
 public class SelfTest {
     private boolean testPassed = false;
 
-    public void runSelfTest(Thermometer thermometer){
+    public void runSelfTest(){
         this.testPassed = true;
-        thermometer.produceBeepingSound(3);
-        System.out.println("Temperature is ready to be read");
     }
 
     public boolean isTestPassed() {
